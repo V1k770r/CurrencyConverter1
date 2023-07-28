3c03ddaf8780d8cb77669c6504aa65bcdb171cff
Victor <testprueba066@gmail.com>
2023-07-27 22:17:48 -0500

    casi terminandoxd

--- src/Conversor.java
+++ src/Conversor.java
@@ -2,19 +2,13 @@ import javax.swing.*;
 
 public class Conversor {
 
-    //valida cantidad ingresada
-
-    public String validarValorNumerico(String cantidad){
+    public void validarValorNumerico(String cantidad){
 
         if(!cantidad.matches("^\\d+(\\.\\d+)?$")){
             JOptionPane.showMessageDialog(null, "Debe ingresar valores numéricos válidos",
                     "Alerta", JOptionPane.ERROR_MESSAGE);
         }
-        return "Procede";
+
     }
-
-
-
-
 
 }
--- src/ConversorGrafico2.java
+++ src/ConversorGrafico2.java
@@ -3,20 +3,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 
 public class ConversorGrafico2 extends JFrame {
 
     public JMenuBar menuBar;
-    public JMenu menu, menu2, menu3, menu4;
+    public JMenu menu, menu2, menu3;
     public JMenuItem opcion1, opcion2, opcion3, opcion4, opcion5, opcion6, opcion7, opcion8,
-                    opcion9, opcion10, opcion11, opcion12, opcion13, opcion14, opcion15, opcion16, opcion17;
+              opcion9, opcion10, opcion11, opcion12, opcion13, opcion14, opcion15, opcion16, opcion17;
 
 
     //creando constructor
     public ConversorGrafico2(){
         setSize(450,100);
-        setTitle("CONVERSOR ORACLE - ONE :)");
+        setTitle("CONVERSOR ORACLE - ONE");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -37,13 +38,13 @@ public class ConversorGrafico2 extends JFrame {
         setLocation(x,y);
     }
 
-    public void mostrarMenus(){
+    public void mostrarMenus() {
         // Inicializacion de los atributos "variables de la clase"
         menuBar = new JMenuBar();
         menu = new JMenu("SELECCIONE UNA OPCION DE CONVERSION");
         menu2 = new JMenu("Conversor de Moneda");
         menu3 = new JMenu("Conversor de Temperatura");
-        menu4 = new JMenu("Salir");
+
 
         //menu2 = new JMenu("Alguna otra conversion?");
         opcion1 = new JMenuItem("Soles a Dolares");
@@ -65,16 +66,13 @@ public class ConversorGrafico2 extends JFrame {
         opcion17 = new JMenuItem("Kelvin a Celcious");
 
 
-
-
-
         //Agregando estilos a las opciones
-        Font myFont1 = new Font("Serif", Font.BOLD, 19);
+        Font myFont1 = new Font("Arial", Font.BOLD, 19);
         Font myFont2 = new Font("Arial", Font.ITALIC, 19);
         menu.setFont(myFont1);
         menu2.setFont(myFont1);
         menu3.setFont(myFont1);
-        menu4.setFont(myFont1);
+
 
         opcion1.setFont(myFont2);
         opcion2.setFont(myFont2);
@@ -87,6 +85,13 @@ public class ConversorGrafico2 extends JFrame {
         opcion9.setFont(myFont2);
         opcion10.setFont(myFont2);
         opcion11.setFont(myFont2);
+        opcion12.setFont(myFont2);
+        opcion13.setFont(myFont2);
+        opcion14.setFont(myFont2);
+        opcion15.setFont(myFont2);
+        opcion16.setFont(myFont2);
+        opcion17.setFont(myFont2);
+
 
         menu2.add(opcion1);
         menu2.add(opcion2);
@@ -111,52 +116,30 @@ public class ConversorGrafico2 extends JFrame {
         menuBar.add(menu);
         menu.add(menu2);
         menu.add(menu3);
-        menu.add(menu4);
-        //menuBar.add(menu2);
+
+        JMenuItem [] options = new JMenuItem[]{opcion1, opcion2, opcion3, opcion4, opcion5, opcion6,
+                opcion7, opcion8, opcion9, opcion10, opcion11, opcion12, opcion13, opcion14, opcion15,
+                opcion16, opcion17};
 
 
+            opcion1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+                    ConversorDeMoneda conversorDeMoneda = new ConversorDeMoneda();
+                    BigDecimal Value = new BigDecimal(inputValue);
+                    String resultadoFinal = conversorDeMoneda.SolesAdolares (Value);
 
+                    JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                    int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
 
+                    if (JOptionPane.OK_OPTION != confirmado){
+                        System.exit(0);
+                    }
 
-//        private void calculandoSolicutud(new JMenuItem(), new ConversorDeMoneda()) {
-//            opcion1.addActionListener(new ActionListener() {
-//                @Override
-//                public void actionPerformed(ActionEvent e) {
-//                    String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
-//
-//                    ConversorDeMoneda conversorDeMoneda = new ConversorDeMoneda();
-//                    conversorDeMoneda.validarValorNumerico(inputValue);
-//
-//                    BigDecimal Value = new BigDecimal(inputValue);
-//                    String resultadoFinal = conversorDeMoneda.SolesAdolares (Value);
-//
-//                    JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
-//                    JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
-//                }
-//            });
-//
-//
-//        }
+                }
+            });
 
-
-
-
-
-        opcion1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
-
-                ConversorDeMoneda conversorDeMoneda = new ConversorDeMoneda();
-                conversorDeMoneda.validarValorNumerico(inputValue);
-
-                BigDecimal Value = new BigDecimal(inputValue);
-                String resultadoFinal = conversorDeMoneda.SolesAdolares (Value);
-
-                JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
-                JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
-            }
-        });
 
         opcion2.addActionListener(new ActionListener() {
             @Override
@@ -171,6 +154,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.SolesAEuros (Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -186,6 +174,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.SolesALibrasSterlinas (Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -201,6 +194,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.SolesAYenJapones (Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -216,6 +214,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.SolesAWonSulKoreano(Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -231,6 +234,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.DolaresASoles(Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -246,6 +254,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.EurosASoles (Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -261,6 +274,11 @@ public class ConversorGrafico2 extends JFrame {
                 String resultadoFinal = conversorDeMoneda.LibrasEsterlinasASoles(Value);
 
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -275,6 +293,11 @@ public class ConversorGrafico2 extends JFrame {
                 BigDecimal Value = new BigDecimal(inputValue);
                 String resultadoFinal = conversorDeMoneda.YenJaponesASoles(Value);
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -289,6 +312,11 @@ public class ConversorGrafico2 extends JFrame {
                 BigDecimal Value = new BigDecimal(inputValue);
                 String resultadoFinal = conversorDeMoneda.WonSulCoreanoASoles(Value);
                 JOptionPane.showMessageDialog(null,"La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
 
@@ -313,13 +341,105 @@ public class ConversorGrafico2 extends JFrame {
                 BigDecimal Value = new BigDecimal(inputValue);
                 String resultadoFinal = conversorDeTemperatura.CelciusAFahrenheit(Value);
                 JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
-                JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
             }
         });
+
+        opcion13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+
+                ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
+                conversorDeTemperatura.validarValorNumerico(inputValue);
+                BigDecimal Value = new BigDecimal(inputValue);
+                String resultadoFinal = conversorDeTemperatura.CelciusAKelvin(Value);
+                JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
+            }
+        });
+
+        opcion14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+
+                ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
+                conversorDeTemperatura.validarValorNumerico(inputValue);
+                BigDecimal Value = new BigDecimal(inputValue);
+                String resultadoFinal = conversorDeTemperatura.FahrenheitACelcius(Value);
+                JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
+            }
+        });
+
+        opcion15.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+
+                ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
+                conversorDeTemperatura.validarValorNumerico(inputValue);
+                BigDecimal Value = new BigDecimal(inputValue);
+                String resultadoFinal = conversorDeTemperatura.FahrenheitAKelvin(Value);
+                JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
+            }
+        });
+
+        opcion16.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+
+                ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
+                conversorDeTemperatura.validarValorNumerico(inputValue);
+                BigDecimal Value = new BigDecimal(inputValue);
+                String resultadoFinal = conversorDeTemperatura.KelvinACelsious(Value);
+                JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
+            }
+        });
+
+        opcion17.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = JOptionPane.showInputDialog("Ingrese la cantidad que desee convertir:");
+
+                ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
+                conversorDeTemperatura.validarValorNumerico(inputValue);
+                BigDecimal Value = new BigDecimal(inputValue);
+                String resultadoFinal = conversorDeTemperatura.KelvinAFahrenheit(Value);
+                JOptionPane.showMessageDialog(null, "La conversion de " + resultadoFinal);
+                int confirmado = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+                if (JOptionPane.OK_OPTION != confirmado){
+                    System.exit(0);
+                }
+            }
+        });
+
     }
-
-
-
 
     public static void main(String[] args) {
         ConversorGrafico2 ventana = new ConversorGrafico2();
